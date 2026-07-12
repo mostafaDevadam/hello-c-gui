b2d333f8b3643bb9dde2c2073a5b9d6680746459
mostafaDevadam <adam.mostafa@outlook.de>
2026-07-13 01:21:12 +0300

    updae motif and create main layout

--- motif.c
+++ motif.c
@@ -4,9 +4,12 @@
 #include <Xm/PushB.h>
 #include <Xm/RowColumn.h>
 #include <Xm/TextF.h>
+#include <Xm/Text.h>
 #include <Xm/Label.h>
 #include <ctype.h>
 #include <Xm/MessageB.h>
+#include <Xm/List.h>
+#include <Xm/Form.h>
 
 typedef struct UserNode {
    char name[100];
@@ -19,6 +22,7 @@ typedef struct {
 	  Widget name_field;
 	  Widget city_field;
 	  Widget age_field;
+      Widget display_list;
       UserData **list_head;
 	} FormFields;
 
@@ -103,21 +107,30 @@ void on_submit(Widget w, XtPointer client_data, XtPointer call_data){
 		       // save data
                UserData *new_user = (UserData *)malloc(sizeof(UserData));
                if(new_user != NULL){
-                snprintf(new_user->name, sizeof(new_user->name), "%s", name);
-                snprintf(new_user->city, sizeof(new_user->city), "%s", city);
-                new_user->age = age;
+                    snprintf(new_user->name, sizeof(new_user->name), "%s", name);
+                    snprintf(new_user->city, sizeof(new_user->city), "%s", city);
+                    new_user->age = age;
 
-                new_user->next = *(fields->list_head);
-                *(fields->list_head) = new_user;
+                    new_user->next = *(fields->list_head);
+                    *(fields->list_head) = new_user;
 
-                printf("Success saved into list memory!\n");
+                    printf("Success saved into list memory!\n");
 
-               }
+                }
+
+                char display_line[256];
+                snprintf(display_line, sizeof(display_line), "%s (%d) - %s", name, age, city);
+
+                XmString motif_line = XmStringCreateLocalized(display_line);
+
+                XmListAddItem(fields->display_list, motif_line, 0);
+                XmStringFree(motif_line);
+
 
                XmTextFieldSetString(fields->name_field, "");
-               
-               
-               
+               XmTextFieldSetString(fields->age_field, "");
+               XmTextFieldSetString(fields->city_field, "");
+
                // open dialog on screen
                XtManageChild(dialog);
            
@@ -149,7 +162,7 @@ int main(int argc, char *argv[]) { // Added standard arguments required by initi
     UserData *user_list_head = NULL;
 
     XtAppContext app;
-    Widget top_level, row_col, button,lbl1, lbl2, lbl3, text_field, submit_btn;
+    Widget top_level, row_col,main_form, main_content_window, left_sidebar, lbl_right, details_box, center_window, right_sidebar, sidebar, button,lbl1, lbl2, lbl3, text_field, submit_btn;
     
     FormFields *my_form = (FormFields *)malloc(sizeof(FormFields));
 
@@ -163,29 +176,44 @@ int main(int argc, char *argv[]) { // Added standard arguments required by initi
         NULL);
 
     //XtVaSetValues(top_level, XmNwidth, 1000, XmNheight, 700, NULL);
+
+    main_form = XmCreateForm(top_level, "mainForm", NULL, 0);
+    XtManageChild(main_form);
+
     
-    row_col = XmCreateRowColumn(top_level, "mainLayout", NULL, 0);
-    XtManageChild(row_col);
+    
+    left_sidebar = XmCreateRowColumn(main_form, "mainLayout", NULL, 0);
+    XtVaSetValues(
+        left_sidebar,
+        XmNtopAttachment, XmATTACH_FORM,
+        XmNbottomAttachment, XmATTACH_FORM,
+        XmNleftAttachment, XmATTACH_FORM,
+        XmNwidth, 200,
+        NULL
+    );
+    XtManageChild(left_sidebar);
+
+
     
     // text_field
     //text_field = XmCreateTextField(row_col, "inputField", NULL, 0);
     //XtManageChild(text_field);
     
     
-    lbl1 = XmCreateLabel(row_col, "Name:", NULL, 0);
+    lbl1 = XmCreateLabel(left_sidebar, "Name:", NULL, 0);
     XtManageChild(lbl1);
-    my_form->name_field = XmCreateTextField(row_col, "Name", NULL, 0);
+    my_form->name_field = XmCreateTextField(left_sidebar, "Name", NULL, 0);
     XtManageChild(my_form->name_field);
     
-    lbl2 = XmCreateLabel(row_col, "Age:", NULL, 0);
+    lbl2 = XmCreateLabel(left_sidebar, "Age:", NULL, 0);
     XtManageChild(lbl2);
-    my_form->age_field = XmCreateTextField(row_col, "Age", NULL, 0);
+    my_form->age_field = XmCreateTextField(left_sidebar, "Age", NULL, 0);
     XtManageChild(my_form->age_field);
     
     
-    lbl3 = XmCreateLabel(row_col, "City:", NULL, 0);
+    lbl3 = XmCreateLabel(left_sidebar, "City:", NULL, 0);
     XtManageChild(lbl3);
-    my_form->city_field = XmCreateTextField(row_col, "City", NULL, 0);
+    my_form->city_field = XmCreateTextField(left_sidebar, "City", NULL, 0);
     XtManageChild(my_form->city_field);
     
     
@@ -193,15 +221,61 @@ int main(int argc, char *argv[]) { // Added standard arguments required by initi
     
     
     // 3. Fixed the variable typo 'o' to the digit '0'
-    button = XmCreatePushButton(row_col, "Click", NULL, 0);
+    button = XmCreatePushButton(left_sidebar, "Click", NULL, 0);
     XtManageChild(button);
     XtAddCallback(button, XmNactivateCallback, button_clicked, NULL);
     
-    submit_btn = XmCreatePushButton(row_col, "Submit", NULL, 0);
+    submit_btn = XmCreatePushButton(left_sidebar, "Submit", NULL, 0);
     XtManageChild(submit_btn);
     XtAddCallback(submit_btn, XmNactivateCallback, on_submit, (XPointer)my_form);
     //XtAddCallback(submit_btn, XmNactivateCallback, submit_clicked, (XPointer)text_field);
     
+
+    right_sidebar = XmCreateRowColumn(main_form, "rightSidebar", NULL, 0);
+    XtVaSetValues(
+        right_sidebar,
+        XmNtopAttachment, XmATTACH_FORM,
+        XmNbottomAttachment, XmATTACH_FORM,
+        XmNrightAttachment, XmATTACH_FORM,
+        XmNwidth, 220,
+        NULL
+    );
+    XtManageChild(right_sidebar);
+
+    lbl_right = XmCreateLabel(right_sidebar, "Notes", NULL, 0);
+    XtManageChild(lbl_right);
+    details_box = XmCreateScrolledText(right_sidebar, "detailsBox", NULL, 0);
+    XtVaSetValues(details_box, XmNeditMode, XmMULTI_LINE_EDIT, XmNrows, 10, NULL);
+    XtManageChild(details_box);
+
+
+
+    my_form->display_list = XmCreateScrolledList(main_form, "userListView", NULL, 0);
+    center_window = XtParent(my_form->display_list);
+    //XtVaSetValues(my_form->display_list, XmNvisibleItemCount, 8, NULL);
+    XtVaSetValues(
+      center_window,
+      XmNtopAttachment, XmATTACH_FORM,
+      XmNbottomAttachment, XmATTACH_FORM,
+      XmNleftAttachment, XmATTACH_WIDGET,
+      XmNleftWidget, left_sidebar,
+      XmNleftOffset, 10,
+      XmNrightAttachment, XmATTACH_WIDGET,
+      XmNrightWidget, right_sidebar,
+      XmNrightOffset, 10,
+
+      NULL
+    );
+    
+    
+    XtManageChild(my_form->display_list);
+
+    
+
+
+
+
+
     XtRealizeWidget(top_level);
     XtAppMainLoop(app);
     
